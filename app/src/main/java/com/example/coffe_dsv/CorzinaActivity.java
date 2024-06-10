aa59c9cc44c3b5a3bdce994b4a69873c8d30bc76
Joe6355 <dudin1918@mail.ru>
2024-06-10 10:00:20 +0500

    Сделал:      -теперь если корзина пустая у нас выводится тоаст с прделожением ПОЙТИ НАХУЙ      -товары корректно добавляются в корзину      -если в корзине что-то есть, хотя бы одна позиция у нас вылезает куаркод

--- app/src/main/java/com/example/coffe_dsv/CorzinaActivity.java
+++ app/src/main/java/com/example/coffe_dsv/CorzinaActivity.java
@@ -1,10 +1,16 @@
 package com.example.coffe_dsv;
-
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class CorzinaActivity extends AppCompatActivity {
+
+    private ImageView previewImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +20,37 @@ public class CorzinaActivity extends AppCompatActivity {
         ListView ordersList = findViewById(R.id.order_list);
         CartAdapter adapter = new CartAdapter(this, Order.items);
         ordersList.setAdapter(adapter);
+
+        previewImage = findViewById(R.id.previewImage);
+
+        // Обработчик нажатия на кнопку
+        Button imageButton = findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Проверяем, пуста ли корзина
+                if (Order.items.isEmpty()) {
+                    // Если корзина пуста, показываем сообщение через Toast
+                    Toast.makeText(CorzinaActivity.this, "Добавьте что-нибудь в корзину", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Если в корзине есть элементы, показываем изображение
+                    previewImage.setImageResource(R.drawable.oplata);
+                    previewImage.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        // Обработчик нажатия на изображение
+        previewImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Скрываем изображение при нажатии на него
+                previewImage.setVisibility(View.GONE);
+            }
+        });
     }
 }
+
+
+
+
